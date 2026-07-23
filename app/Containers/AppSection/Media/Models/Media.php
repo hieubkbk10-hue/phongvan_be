2e431aedf4726025647771f9acc79f3b114d8c40
hieubkbk10-hue <252451999+hieubkbk10-hue@users.noreply.github.com>
2026-07-23 16:09:58 +0700

    feat: create Media model with morph relation and fillable attributes

--- app/Containers/AppSection/Media/Models/Media.php
+++ app/Containers/AppSection/Media/Models/Media.php
@@ -3,23 +3,41 @@
 namespace App\Containers\AppSection\Media\Models;
 
 use App\Ship\Parents\Models\Model as ParentModel;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Media extends ParentModel
 {
-    protected $fillable = [
+    protected $table = 'media';
 
+    protected $fillable = [
+        'name',
+        'file_path',
+        'disk',
+        'mime_type',
+        'size',
+        'mediable_type',
+        'mediable_id',
     ];
 
     protected $hidden = [
-
     ];
 
     protected $casts = [
-
+        'size' => 'integer',
     ];
 
     /**
      * A resource key to be used in the serialized responses.
      */
     protected string $resourceKey = 'Media';
+
+    public static function getTableName()
+    {
+        return 'media';
+    }
+
+    public function mediable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
