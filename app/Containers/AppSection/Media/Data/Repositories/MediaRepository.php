750cb0457798d4dde7d638ec680ef250d540aa8f
hieubkbk10-hue <252451999+hieubkbk10-hue@users.noreply.github.com>
2026-07-23 16:24:51 +0700

    feat: implement MediaRepository with search configuration for Media model

--- app/Containers/AppSection/Media/Data/Repositories/MediaRepository.php
+++ app/Containers/AppSection/Media/Data/Repositories/MediaRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\AppSection\Media\Data\Repositories;
 
+use App\Containers\AppSection\Media\Models\Media;
 use App\Ship\Parents\Repositories\Repository as ParentRepository;
 
 class MediaRepository extends ParentRepository
@@ -10,7 +11,13 @@ class MediaRepository extends ParentRepository
      * @var array
      */
     protected $fieldSearchable = [
-        'id' => '=',
-        // ...
+        'mediable_type' => '=',
+        'is_main' => '=',
+        'sort_order' => '=',
     ];
+
+    public function model(): string
+    {
+        return Media::class;
+    }
 }
