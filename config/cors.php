6d7d0d7c5dadb2f1b5c91daab983eeb128472d5f
hieubkbk10-hue <252451999+hieubkbk10-hue@users.noreply.github.com>
2026-07-23 11:49:22 +0700

    config: update cors allowed origins and enable supports_credentials for frontend

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://phongvan-fe.vitrasau.info.vn',
+        'https://phongvan-fe.vitrasau.info.vn',
+        'http://localhost:5173',
+        'http://localhost:3000',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +34,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
