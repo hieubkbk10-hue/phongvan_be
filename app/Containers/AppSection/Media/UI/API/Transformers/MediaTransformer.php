1a8a3a697c70f95d987ff0352545f18284221af1
hieubkbk10-hue <252451999+hieubkbk10-hue@users.noreply.github.com>
2026-07-23 17:14:18 +0700

    feat: add MediaTransformer to format media model attributes for API responses

--- app/Containers/AppSection/Media/UI/API/Transformers/MediaTransformer.php
+++ app/Containers/AppSection/Media/UI/API/Transformers/MediaTransformer.php
@@ -8,27 +8,35 @@ use App\Ship\Parents\Transformers\Transformer as ParentTransformer;
 class MediaTransformer extends ParentTransformer
 {
     protected array $defaultIncludes = [
-
     ];
 
     protected array $availableIncludes = [
-
     ];
 
     public function transform(Media $media): array
     {
+        /** @psalm-suppress UndefinedFunction */
+        $url = storage_url($media->path, $media->disk);
+
         $response = [
             'object' => $media->getResourceKey(),
             'id' => $media->getHashedKey(),
+            'url' => $url,
+            'filename' => $media->filename,
+            'mime_type' => $media->mime_type,
+            'size' => $media->size,
+            'sort_order' => $media->sort_order,
+            'is_primary' => $media->is_main,
         ];
 
         return $this->ifAdmin([
             'real_id' => $media->id,
+            'disk' => $media->disk,
+            'path' => $media->path,
             'created_at' => $media->created_at,
             'updated_at' => $media->updated_at,
-            'readable_created_at' => $media->created_at->diffForHumans(),
-            'readable_updated_at' => $media->updated_at->diffForHumans(),
-            // 'deleted_at' => $media->deleted_at,
+            'readable_created_at' => $media->created_at?->diffForHumans(),
+            'readable_updated_at' => $media->updated_at?->diffForHumans(),
         ], $response);
     }
 }
